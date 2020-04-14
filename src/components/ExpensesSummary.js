aadca2e21f0930b0388b2a14a2c837c4b3154bf2
matussovcik <sovcikmatus@gmail.com>
2020-04-14 18:12:07 +0200

    Added numeral formatting to ExpensesSummary.js

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expensesTotal';
 
 export const ExpensesSummary = (props) => (
   <div>
-    <p>Viewing {props.expenses.length} {props.expenses.length === 1 ? 'expense' : 'expenses'} totalling: {selectExpensesTotal(props.expenses)}.</p>
+    <p>Viewing {props.expenses.length} {props.expenses.length === 1 ? 'expense' : 'expenses'} totalling: {numeral(selectExpensesTotal(props.expenses)).format('$0,0.00')}.</p>
   </div>
 );
 
